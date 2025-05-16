3cfd05d69d6ada2d373987f3606b3fd5f84ed451
Gabriel <gm888641@gmail.com>
2025-05-16 01:01:40 -0300

    Se a añadido que el jugador pueda moverse de izquierda y derecha

--- hollow_knight_moviento/Player/Scripts/player_controller.gd
+++ hollow_knight_moviento/Player/Scripts/player_controller.gd
@@ -6,6 +6,8 @@ var movement = Vector2()
 
 func _physics_process(delta):
 	horizontal_movement()
+	set_animations()
+	flip()
 	
 	move_and_slide()
 
@@ -16,3 +18,15 @@ func horizontal_movement():
 		velocity.x = movement * move_speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, move_speed * deceleration)
+
+func set_animations():
+	if velocity.x != 0:
+		$anim.play("Move")
+	if velocity.x == 0:
+		$anim.play("Idle")
+
+func flip():
+	if velocity.x > 0.0:
+		scale.x = scale.y * 1
+	if velocity.x < 0.0:
+		scale.x = scale.y * -1
