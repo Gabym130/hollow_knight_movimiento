81589d47707a85c7cdd9051c55db23ce752f0a7b
Gabriel <gm888641@gmail.com>
2025-05-16 01:30:24 -0300

    Se ha añadido salto al jugador

--- hollow_knight_moviento/Player/Scripts/player_controller.gd
+++ hollow_knight_moviento/Player/Scripts/player_controller.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 
+@export_category("Movement variable")
 @export var move_speed = 120.0
 @export var deceleration = 0.1
 @export var gravity = 500.0
 var movement = Vector2()
 
+@export_category("Jump variable")
+@export var jump_speed = 190.0
+@export var acceleration = 290.0
+@export var jump_amount = 2
+
 func _physics_process(delta):
 	velocity.y  += gravity * delta
 	
 	horizontal_movement()
+	jump_logic()
 	set_animations()
 	flip()
 	
@@ -33,3 +40,11 @@ func flip():
 		scale.x = scale.y * 1
 	if velocity.x < 0.0:
 		scale.x = scale.y * -1
+
+
+func jump_logic():
+	if is_on_floor():
+		jump_amount = 2
+		if Input.is_action_just_pressed("ui_accept"):
+			jump_amount -= 1
+			velocity.y -= lerp(jump_speed, acceleration, 0.1)
