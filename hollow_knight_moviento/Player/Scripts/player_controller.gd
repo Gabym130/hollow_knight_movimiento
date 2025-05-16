9d47f6ac72f174d29b1dca30a2903f28912687bf
Gabriel <gm888641@gmail.com>
2025-05-16 02:45:03 -0300

    Saltar en la paredes añadido

--- hollow_knight_moviento/Player/Scripts/player_controller.gd
+++ hollow_knight_moviento/Player/Scripts/player_controller.gd
@@ -11,23 +11,33 @@ var movement = Vector2()
 @export var acceleration = 290.0
 @export var jump_amount = 2
 
+@export_category("Wall jump variable")
+@export var wall_slide = 10
+@onready var left_ray = $raycast/left_ray
+@onready var right_ray = $raycast/right_ray
+@export var wall_x_force = 200.0
+@export var wall_y_force = -220.0
+var is_wall_jumping = false
+
 func _physics_process(delta):
 	velocity.y  += gravity * delta
 	
 	horizontal_movement()
 	jump_logic()
+	wall_logic()
 	set_animations()
 	flip()
 	
 	move_and_slide()
 
 func horizontal_movement():
-	movement = Input.get_axis("ui_left", "ui_right")
-	
-	if movement:
-		velocity.x = movement * move_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, move_speed * deceleration)
+	if is_wall_jumping == false:
+		movement = Input.get_axis("ui_left", "ui_right")
+		
+		if movement:
+			velocity.x = movement * move_speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, move_speed * deceleration)
 
 func set_animations():
 	if velocity.x != 0:
@@ -42,8 +52,10 @@ func set_animations():
 func flip():
 	if velocity.x > 0.0:
 		scale.x = scale.y * 1
+		wall_x_force = 200.0
 	if velocity.x < 0.0:
 		scale.x = scale.y * -1
+		wall_x_force = -200.0
 
 
 func jump_logic():
@@ -64,3 +76,20 @@ func jump_logic():
 				velocity.y *= 0.3
 	else:
 		return
+
+func wall_logic():
+	if is_on_wall_only():
+		velocity.y = 10
+		if Input.is_action_just_pressed("ui_accept"):
+			if left_ray.is_colliding():
+				velocity = Vector2(wall_x_force, wall_y_force)
+				wall_jumping()
+			if right_ray.is_colliding():
+				velocity = Vector2(-wall_x_force, wall_y_force)
+				wall_jumping()
+				
+				
+func wall_jumping():
+	is_wall_jumping = true
+	await get_tree().create_timer(0.12).timeout
+	is_wall_jumping = false
