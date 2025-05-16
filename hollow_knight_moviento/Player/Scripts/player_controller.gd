2afea68ad7851d103e68bc54069b2e816a96eb20
Gabriel <gm888641@gmail.com>
2025-05-16 00:44:06 -0300

    Se ha añadido un script al jugador

--- hollow_knight_moviento/Player/Scripts/player_controller.gd
+++ hollow_knight_moviento/Player/Scripts/player_controller.gd
@@ -1,1 +1,18 @@
 extends CharacterBody2D
+
+@export var move_speed = 120.0
+@export var deceleration = 0.1
+var movement = Vector2()
+
+func _physics_process(delta):
+	horizontal_movement()
+	
+	move_and_slide()
+
+func horizontal_movement():
+	movement = Input.get_axis("ui_left", "ui_right")
+	
+	if movement:
+		velocity.x = movement * move_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, move_speed * deceleration)
